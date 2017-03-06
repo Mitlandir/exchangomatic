f3ef2bedb5dbb2c218431156c534a82f700c21df
mitlandir <duljarevic@gmail.com>
2017-03-06 23:51:03 +0100

    nova izmena

---
+++ src/java/learninggit/Dejan.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package learninggit;
+
+/**
+ *
+ * @author Dejan
+ */
+public class Dejan {
+    
+    
+    public String izmena(){
+        return "izmena!";
+    }
+    
+    
+}
